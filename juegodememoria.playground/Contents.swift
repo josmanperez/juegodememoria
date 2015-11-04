b5dbcda7624e877eb50804638d004e15a11c60f0
Josman Pérez Expósito <Josman@MacBookPro-Josman.local>
2015-11-04 23:33:13 +0100

    Subido archivo correspondiente al primer assignment

---
+++ juegodememoria.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/** Assignment numero 1
+ *  JUEGO DE MEMORIA
+ */
+for i in 1...100 {
+  if (i%5==0) {
+    print("#\(i)\t Bingo!!!")
+  }
+  if (i%2==0) {
+    print("#\(i)\t Par!!!")
+  } else {
+    print("#\(i)\t Impar!!!")
+  }
+  if (i>=30 && i<=40) {
+    print("#\(i)\t Viva Swift!!!")
+  }
+  
+
+}
